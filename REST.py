99e8e08286870780cf202a1ea51f1556dff375a1
František Jeřábek <fanajerabek@seznam.cz>
2021-11-02 16:35:26 +0100

    Added chunks insert to database to all requests

--- REST.py
+++ REST.py
@@ -1,10 +1,8 @@
 import datetime
-import json
 from urllib.request import urlopen
 
 import ijson
 import pymongo.collection
-import requests
 from pymongo import MongoClient
 
 client = MongoClient("localhost", 27017)
@@ -13,59 +11,52 @@ client = MongoClient("localhost", 27017)
 def monthly_stats():
     print("==========infected monthly==========")
     url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/nakazeni-vyleceni-umrti-testy.json"
-    json_data = get_data(url)
-    print("Saving database")
     collection: pymongo.collection.Collection = client.upa.monthlyStats
     collection.delete_many({})
-    collection.insert_many(json_data["data"])
+
+    f = urlopen(url)
+    people = ijson.items(f, 'data.item')
+    for person in people:
+        fix_date_item(person)
+        collection.insert_one(person)
+
     print("==========completed==========")
 
 
 def people_region_infected_stats():
     print("==========infected in regions==========")
-    url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/osoby.json"
-    json_data = get_data(url)
-    print("Saving to database")
+    url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/osoby.min.json"
     collection: pymongo.collection.Collection = client.upa.peopleRegionInfected
     collection.delete_many({})
-    collection.insert_many(json_data["data"])
+
+    f = urlopen(url)
+    people = ijson.items(f, 'data.item')
+    insert_to_db(people, collection)
     print("==========completed==========")
 
 
 def people_vaccinated_region_stats():
     print("==========vaccinated people in regions==========")
-    # url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani.min.json"
-    url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani-profese.min.json"
-    json_data = get_data(url)
-    print("Saving to database")
     collection: pymongo.collection.Collection = client.upa.regionVaccinated
     collection.delete_many({})
-    collection.insert_many(json_data["data"])
+
+    url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani.min.json"
+    f = urlopen(url)
+    people = ijson.items(f, 'data.item')
+    insert_to_db(people, collection)
     print("==========completed==========")
 
 
 def people_vaccinated_all():
-    print("==========vaccinated people in regions==========")
+    print("==========all vaccinated people==========")
     collection: pymongo.collection.Collection = client.upa.peopleVaccinated
     collection.delete_many({})
 
     url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani-profese.min.json"
     f = urlopen(url)
     people = ijson.items(f, 'data.item')
-    i = 0
-    for person in people:
-        i += 1
-        if i % 1000 == 0:
-            print(i)
-        fix_date_item(person)
-        collection.insert_one(person)
+    insert_to_db(people, collection)
     print("==========completed==========")
-
-
-def fix_date(json_data):
-    for data in json_data["data"]:
-        date = datetime.date.fromisoformat(data["datum"])
-        data["datum"] = datetime.datetime(date.year, date.month, date.day)
 
 
 def fix_date_item(item):
@@ -73,21 +64,22 @@ def fix_date_item(item):
     item["datum"] = datetime.datetime(date.year, date.month, date.day)
 
 
-def get_data(url):
-    print("Sending GET")
-    response = requests.get(url)
-    print("Parsing json")
-    json.loads()
-    json_data = response.json()
+def insert_to_db(iterable, collection, chunk=100000):
+    people_chunk = []
+    index = 0
+    for item in iterable:
+        if index % chunk == 1:
+            print(index)
+            collection.insert_many(people_chunk)
+            people_chunk.clear()
+        fix_date_item(item)
+        people_chunk.append(item)
 
-    print("Fixing date")
-    fix_date(json_data)
-
-    return json_data
+    collection.insert_many(people_chunk)
 
 
 if __name__ == '__main__':
-    # monthly_stats()
-    # people_region_infected_stats()
-    # people_vaccinated_region_stats()
+    monthly_stats()
+    people_region_infected_stats()
+    people_vaccinated_region_stats()
     people_vaccinated_all()
