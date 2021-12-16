9d83e1f8645ae3192d09f82db80b2c5bf5b07f6d
František Jeřábek <fanajerabek@seznam.cz>
2021-12-16 16:58:21 +0100

    Uncommented methods

--- download.py
+++ download.py
@@ -149,11 +149,11 @@ def age_distribution():
 
 
 if __name__ == '__main__':
-    # monthly_stats()
-    # people_region_infected_stats()
-    # people_vaccinated_region_stats()
-    # people_vaccinated_all()
-    # total_population()
-    # hospitalized()
+    monthly_stats()
+    people_region_infected_stats()
+    people_vaccinated_region_stats()
+    people_vaccinated_all()
+    total_population()
+    hospitalized()
     age_distribution()
 
